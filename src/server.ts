84872ba74b3d069e5c4243cf81a00d18a50b239f
Alejandro <aavila@bemobile.es>
2023-12-18 11:27:52 +0100

    refactor: add helmet & route prefix

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,17 @@ import Fastify from "fastify"
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import {swaggerOptions, swaggerUiOptions} from "./docs/swagger";
-import {userRoutes} from "./api/routes/user.routes";
+import fastifyHelmet from "@fastify/helmet";
+import {routes} from "./api/routes/user.routes";
 
 export const fastify = Fastify({
     logger: {level: "debug"}
 })
-
+fastify.register(fastifyHelmet, {global: true})
 fastify.register(fastifySwagger, swaggerOptions);
 fastify.register(fastifySwaggerUi, swaggerUiOptions);
 
-fastify.register(userRoutes)
+fastify.register(routes, {prefix: "/api/v1/users"})
 
 const startServer = async () => {
     try {
